58baeef2ee832362ecb658ad516dae827a696a6a
meo <lupomeo@hotmail.com>
2023-03-23 16:56:09 +0100

    first import

---
+++ calendarapp/views/__init__.py
@@ -0,0 +1,31 @@
+from .event_list import AllEventsListView, RunningEventsListView
+from .other_views import (
+    CalendarViewNew,
+    CalendarView,
+    create_event,
+    EventEdit,
+    event_details,
+    add_eventmember,
+    EventMemberDeleteView,
+    createevent,
+    getevents,
+    editevent,
+    deleteevent,
+)
+
+
+__all__ = [
+    AllEventsListView,
+    RunningEventsListView,
+    CalendarViewNew,
+    CalendarView,
+    create_event,
+    createevent,
+    getevents,
+    editevent,
+    deleteevent,
+    EventEdit,
+    event_details,
+    add_eventmember,
+    EventMemberDeleteView,
+]
